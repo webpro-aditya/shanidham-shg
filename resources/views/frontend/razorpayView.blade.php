bfe1f75c94e2be746e94b7be6e88450b186ea2d7
Aditya D <webpro.aditya@gmail.com>
2025-03-27 19:56:02 +0530

    mdy: razorpay view

--- resources/views/frontend/razorpayView.blade.php
+++ resources/views/frontend/razorpayView.blade.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
-      <title>Rajbala SHG</title>
+      <title>Shanidham SHG</title>
       
       <!-- jQuery -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" crossorigin="anonymous"></script>
